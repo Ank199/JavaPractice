8908c3bb9a13870e50dd3fb0421d17cc3640c602
Ankit <ankitlad1999@gmail.com>
2023-08-30 23:46:29 +0530

    files added

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+public class Arithmetic{
+public static void main(String[] args){
+//Addition
+int a = 28;
+int b = 2;
+System.out.println(a + b);
+// Substraction
+System.out.println(a-2);
+// Multiplication
+System.out.println(b * 2);
+// Division
+System.out.println(a/2);
+// Remainder
+System.out.println(a % 3);
+  }
+}
---
+++ swapping.java
@@ -0,0 +1,17 @@
+public class swapping{
+public static void main(String[] args){
+int a=7;
+int b=9;
+System.out.println("value before swapping");
+System.out.println(a);
+System.out.println(b);
+
+int change = a;
+a=b;
+b=change;
+
+System.out.println("value After swapping");
+System.out.println(a);
+System.out.println(b);
+}
+}
